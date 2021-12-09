6eb598b1ba623a151a5a74615579138032b178ce
NROCKZ <narendranjanu2000@gmail.com>
2021-12-09 22:27:31 +0530

    this is my first commit

--- src/other Pages/home.js
+++ src/other Pages/home.js
@@ -2,33 +2,36 @@ import React, { Component } from "react";
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-import {button} from 'react-bootstrap';
+//import {button} from 'react-bootstrap';
 class Home extends Component {
     state={
         news:[]
     }
     componentDidMount(){
-        axios.get("https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=c27efdbe42c54c6a884d79171747d575")
-        .then(app=> this.setState({news:app.data["articles"]}))
+        axios.get("https://api.webz.io/filterWebContent?token=af02a8c7-b854-4a12-95b3-99a22d41e461&q=site_type:news%20country:IN%20site_category:financial_news")
+        .then(app=> this.setState({news:app.data["posts"]}))
 
     }
     render() { 
-        return (<div>
+        return (<div  className="container-sm">
 
-            <div className="display-4 center">
+            <div className="h2">
                 Todays Stories
             </div>
+            <br></br>
+            <br></br>
+            {console.log(this.state.news)}
             {this.state.news.map(app => 
             <div key={app.id}>
                 <div class="jumbotron">
                     <table>
                         <tr>
                             <td>
-                                <img src={app.urlToImage} alt-text="hello" width="400px" />
+                                <img src={app.main_image} alt="news.logo" width="400px" />
                             </td>
                             <td>
                                 <p className="lead">{(app.title)}</p>
-                                <p>{(app.description)}</p>
+                                <p>{(app.text).substring(0,500)}...</p>
                                 <a href={app.url} class="btn btn-primary">Read More</a> 
                             </td>
                         </tr>
